3769d06a10147f13a9173a68dbf68bda13d3a850
Ming Ki Chong <git@mingkichong.net>
2022-02-03 01:03:10 -0800

    styling

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,20 @@
-import * as E from "fp-ts/Either";
-import { pipe } from "fp-ts/pipeable";
+import * as E from 'fp-ts/Either';
+import { pipe } from 'fp-ts/pipeable';
 import * as t from 'io-ts';
-import { failure } from "io-ts/PathReporter";
+import { failure } from 'io-ts/PathReporter';
 
 // https://www.azavea.com/blog/2020/10/29/run-time-type-checking-in-typescript-with-io-ts/
 
-function decode<A, O = A, I = unknown> (codec: t.Type<A, O, I>, maybeId: I){
-    return pipe(codec.decode(maybeId), E.getOrElseW((errors) => { throw new Error(failure(errors).join("\n")) }));
-}
+const decode = <A, O = A, I = unknown>(codec: t.Type<A, O, I>, input: I) =>
+  pipe(input, codec.decode, E.getOrElseW((errors) => { throw new Error(failure(errors).join("\n")) }));
 
 const id = 1234567890;
 const idString = 'abcdefgh';
 
-interface IdBrand {
-  readonly Id: unique symbol;
-}
-
 const IdBrand = t.brand(
   t.number,
-  (n): n is t.Branded<number, IdBrand> => typeof n === 'number',
-  "Id"
+  (n): n is t.Branded<number, { readonly Id: unique symbol; }> => typeof n === 'number',
+  'Id'
 );
 
 console.log(IdBrand.decode(id));
